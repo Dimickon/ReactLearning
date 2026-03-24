856d1f0335fb10edbaaf8b47deec09990a382f17
Dimickon <dimickon1@gmail.com>
2026-03-24 17:16:13 +0300

    complete 13 props

--- 12-reusable-components/src/App.js
+++ 12-reusable-components/src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import MyComponent from './Components/MyComponent';
 import OtherComponent from './Components/OtherComponent';
+import PetInfo from './Components/PetInfo';
 
 function App() {
   return (
     <div className="App">
       <MyComponent />
       <OtherComponent />
+      <PetInfo animal="cat" age={23} />
+      <PetInfo animal="dog" age={2} />
     </div>
   );
 }
