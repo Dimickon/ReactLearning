bebea104a56b1170450897cf224c926aecbe38fd
Dimickon <dimickon1@gmail.com>
2026-03-31 17:43:00 +0300

    todo app v0.0.6

--- 28-todo-app-v1/src/components/Todos/TodoItem.js
+++ 28-todo-app-v1/src/components/Todos/TodoItem.js
@@ -1,9 +1,12 @@
+import { RiTodoFill } from 'react-icons/ri';
 import styles from './TodoItem.module.css';
 
 export default function TodoItem({ index, todo, deleteTodo }) {
   return (
     <div className={styles.todo__item} onDoubleClick={() => deleteTodo(index)}>
-      <div className="todo__icon">1</div>
+      <div className="todo__icon">
+        <RiTodoFill />
+      </div>
       <div className="todo__name">{todo}</div>
     </div>
   );
