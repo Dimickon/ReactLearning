fe244fe3dbebbb09da7742c194395a0329572fe1
Dimickon <dimickon1@gmail.com>
2026-04-03 13:20:40 +0300

    add styles for todoItem

--- 29-todo-app-v2/src/components/Todos/TodoItem.js
+++ 29-todo-app-v2/src/components/Todos/TodoItem.js
@@ -1,16 +1,27 @@
-import { RiTodoFill } from 'react-icons/ri';
+import { RiTodoFill, RiDeleteBin2Line } from 'react-icons/ri';
+import { FaCheck } from 'react-icons/fa';
 import styles from './TodoItem.module.css';
 
 export default function TodoItem({ todo, deleteTodo }) {
   return (
     <div
-      className={styles.todo__item}
+      className={[styles.todoItem].join(' ')}
       onDoubleClick={() => deleteTodo(todo.id)}
     >
-      <div className="todo__icon">
-        <RiTodoFill />
+      <div className={styles.todoMain}>
+        <div className={styles.todoHeading}>
+          <RiTodoFill className={styles.todoIcon} />
+          <div className={styles.todoName}>{todo.text}</div>
+        </div>
       </div>
-      <div className="todo__name">{todo.text}</div>
+      <div className={styles.todoControll}>
+        <RiDeleteBin2Line
+          className={[styles.todoIcon, styles.todoIconDelete].join(' ')}
+        />
+        <FaCheck
+          className={[styles.todoIcon, styles.todoIconComplete].join(' ')}
+        />
+      </div>
     </div>
   );
 }
