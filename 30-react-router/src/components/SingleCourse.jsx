bb3451253012f21e509a0d078ac97aacd015c839
Dimickon <dimickon1@gmail.com>
2026-04-05 19:06:08 +0300

    Использование относительных путей при редиректе

--- 30-react-router/src/components/SingleCourse.jsx
+++ 30-react-router/src/components/SingleCourse.jsx
@@ -1,24 +1,35 @@
-import { Link, useParams } from 'react-router-dom';
+import { Link, useNavigate, useParams } from 'react-router-dom';
 import courses from '../data/courses';
-import NotFound from './NotFound';
+//import NotFound from './NotFound';
+import { useEffect } from 'react';
 
 const SingleCourse = () => {
   const params = useParams();
+  const navigate = useNavigate();
   const course = courses.find((course) => course.slug === params.courseSlug);
-  if (!course) {
-    return (
-      <>
-        <NotFound />
-        <Link to=".." relative="path">
-          All courses
-        </Link>
-      </>
-    );
-  }
+
+  useEffect(() => {
+    if (!course) {
+      navigate('..', { relative: 'path' });
+    }
+  }, [course, navigate]);
+  // Simple show NotFound component
+  // if (!course) {
+  //     return (
+  //       <>
+  //         <NotFound />
+  //         <Link to=".." relative="path">
+  //           All courses
+  //         </Link>
+  //       </>
+  //     );
+  //   }
+
+  // course? - нужно для того, чтобы undefined не обрабатывался
   return (
     <>
-      <h1>{course.title}</h1>
-      <h3>{course.slug}</h3>
+      <h1>{course?.title}</h1>
+      <h3>{course?.slug}</h3>
       <Link to=".." relative="path">
         All courses
       </Link>
