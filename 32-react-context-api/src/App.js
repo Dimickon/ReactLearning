937c865d686acd9208e336e70a161448ee0dd808
Dimickon <dimickon1@gmail.com>
2026-04-06 11:46:09 +0300

    обозначение проблемы передачи параметров

---
+++ 32-react-context-api/src/App.js
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import User from './components/User';
+import './App.css';
+
+function App() {
+  const [user, setUser] = useState('Dima');
+
+  return (
+    <div className="App">
+      <User user={user} />
+    </div>
+  );
+}
+
+export default App;
---
+++ 32-react-context-api/src/components/User.jsx
@@ -0,0 +1,5 @@
+import UserInfo from './UserInfo';
+
+export default function User({ user }) {
+  return <UserInfo user={user} />;
+}
