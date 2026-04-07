747e69702adb3feac828a031fc0d0e34e14b646a
Dimickon <dimickon1@gmail.com>
2026-04-07 13:59:56 +0300

    Добавление новых действий: удаление по имени и очистка всех имён

--- 35-reducer/reducer.js
+++ 35-reducer/reducer.js
@@ -4,5 +4,28 @@ function reducer(state, action) {
   if (action.type === 'ADD_NAME') {
     return [...state, action.payload];
   }
+
+  if (action.type === 'DELETE_NAME') {
+    return state.filter((personName) => personName !== action.payload);
+  }
+
+  if (action.type === 'CLEAR_NAMES') {
+    return [];
+  }
   return state;
 }
+
+let newState = reducer(initialState, { type: 'ADD_NAME', payload: 'Dima' });
+console.log(newState);
+
+newState = reducer(newState, { type: 'ADD_NAME', payload: 'Ksenia' });
+console.log(newState);
+
+newState = reducer(newState, { type: 'DELETE_NAME', payload: 'Dima' });
+console.log(newState);
+
+newState = reducer(newState, { type: 'CLEAR_NAMES' });
+console.log(newState);
+
+newState = reducer(newState, { type: 'ADD_NAME', payload: 'Dima' });
+console.log(newState);
