3a97dc14e5796438dd7c24a8ed6aa7e375b4d8be
Dimickon <dimickon1@gmail.com>
2026-04-08 21:02:28 +0300

    create BookForm components
    Добавлена форма с полями title и author, которые контролируются React

--- 37-react-redux/frontend/src/components/BookForm/BookForm.js
+++ 37-react-redux/frontend/src/components/BookForm/BookForm.js
@@ -1,7 +1,53 @@
+import { useState } from 'react';
+import './BookForm.css';
+
 const BookForm = () => {
+  // const [title, setTitle] = useState('');
+  // const [author, setAuthor] = useState('');
+  const initialStateForm = {
+    title: '',
+    author: '',
+  };
+  const [formData, setFormData] = useState(initialStateForm);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (formData) {
+      console.log(formData);
+    }
+
+    setFormData(initialStateForm);
+  };
+
+  function handleInputChange(text, name) {
+    setFormData({ ...formData, [name]: text });
+  }
+
   return (
     <div className="app-block book-form">
       <h2>Add a New Book</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="title">Title: </label>
+          <input
+            type="text"
+            id="title"
+            value={formData.title}
+            onChange={(e) => handleInputChange(e.target.value, 'title')}
+          />
+        </div>
+        <div>
+          <label htmlFor="author">Author: </label>
+          <input
+            type="text"
+            id="author"
+            value={formData.author}
+            onChange={(e) => handleInputChange(e.target.value, 'author')}
+          />
+        </div>
+        <button type="submit">Add book</button>
+      </form>
     </div>
   );
 };
