86ed6fde88f536261c3861b481502b6f422e5e6f
Dimickon <dimickon1@gmail.com>
2026-04-09 22:01:35 +0300

    Добавили логику считывания ввода в поле фильтра по названию книги
    Использовали Redux Toolkit Slice для обработки input
    
    Можно мутировать состояние за счёт библиотеки Immer, которую использует Redux Toolkit Slice. Эта библиотека формирует новый объект автоматически

--- 37-react-redux/frontend/src/redux/slices/filterSlice.js
+++ 37-react-redux/frontend/src/redux/slices/filterSlice.js
@@ -9,11 +9,16 @@ const filterSlice = createSlice({
   name: 'filter',
   reducers: {
     setTitleFilter: (state, action) => {
-      return { ...state, title: action.payload };
+      /* Можно мутировать состояние за счёт библиотеки Immer, которую использует ReduxSlice. Эта библиотека формирует новый объект автоматически */
+      state.title = action.payload;
+
+      /* Либо формируем новый объект вручную (традиционный подход) */
+      //return {...state, title: action.payload}
     },
   },
 });
 
 /* Автоматическая деструкторизация setTitleFilter вместо const setTitleFilter = filterSlice.actions.setTitleFilter */
 export const { setTitleFilter } = filterSlice.actions;
+export const selectTitleFilter = (state) => state.filter.title;
 export default filterSlice.reducer;
