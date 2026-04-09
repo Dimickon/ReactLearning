34dfa3becde90bb078485a6f7c1732718714f1fb
Dimickon <dimickon1@gmail.com>
2026-04-09 16:19:28 +0300

    Поставил иконки на место и заложил логику для их отображения

--- 37-react-redux/frontend/src/components/BookList/BookList.js
+++ 37-react-redux/frontend/src/components/BookList/BookList.js
@@ -1,4 +1,5 @@
 import { useSelector, useDispatch } from 'react-redux';
+import { BsBookmarkStarFill, BsBookmarkStar } from 'react-icons/bs';
 import { deleteBook } from '../../redux/books/actionCreators';
 import './BookList.css';
 
@@ -22,7 +23,12 @@ const BookList = () => {
               <div className="book-info">
                 {++i}. {book.title} by <strong>{book.author}</strong>
               </div>
-              <div className="boot-actions">
+              <div className="book-actions">
+                {book.isFavorite ? (
+                  <BsBookmarkStarFill className="star-icon" />
+                ) : (
+                  <BsBookmarkStar className="star-icon" />
+                )}
                 <button onClick={() => handleDeleteBook(book.id)}>
                   Delete
                 </button>
