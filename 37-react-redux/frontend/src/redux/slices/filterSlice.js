8c3be19dcc42d4dae6aa1b030736da209382dea4
Dimickon <dimickon1@gmail.com>
2026-04-10 13:33:58 +0300

    Добавили функционал сортировки элементов по избранным книгам

--- 37-react-redux/frontend/src/redux/slices/filterSlice.js
+++ 37-react-redux/frontend/src/redux/slices/filterSlice.js
@@ -3,6 +3,7 @@ import { createSlice } from '@reduxjs/toolkit';
 const initialState = {
   title: '',
   author: '',
+  onlyFavorite: false,
 };
 
 const filterSlice = createSlice({
@@ -20,15 +21,27 @@ const filterSlice = createSlice({
     setAuthorFilter: (state, action) => {
       state.author = action.payload;
     },
-    resetFilters: (state) => {
+    setOnlyFavoriteFilter: (state) => {
+      state.onlyFavorite = !state.onlyFavorite;
+    },
+    resetFilters: () => {
       return initialState;
     },
   },
 });
 
 /* Автоматическая деструкторизация setTitleFilter вместо const setTitleFilter = filterSlice.actions.setTitleFilter */
-export const { setTitleFilter, setAuthorFilter, resetFilters } =
-  filterSlice.actions;
+export const {
+  setTitleFilter,
+  setAuthorFilter,
+  setOnlyFavoriteFilter,
+  resetFilters,
+} = filterSlice.actions;
 export const selectTitleFilter = (state) => state.filter.title;
 export const selectAuthorFilter = (state) => state.filter.author;
+export const selectOnlyFavoriteFilter = (state) => state.filter.onlyFavorite;
+
+/* Если фильтров будет много, то можно всё запихнуть в один экспорт. Но тогда при измении одной части объекта, будут ререндериться все компоненты, в которых используется общий экспорт */
+//export const selectFilters = (state) => state.filters;
+
 export default filterSlice.reducer;
